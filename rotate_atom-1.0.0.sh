1bf7573804da9eb5fe093f3b7e69afba88c2540c
ss-ghule <ss.ghule@ncl.res.in>
2020-03-07 12:26:47 +0530

    * problem during release 1.0.0 * update rotate_atom-1.0.0.sh from tag 1.0.0

--- rotate_atom-1.0.0.sh
+++ rotate_atom-1.0.0.sh
@@ -1,34 +1,27 @@
-input_file_path='/home/vanka/siddharth/molecular_machines_project/molecular_machines/verification/test_systems/ring_track_at_origin_non_ideal_artificial_system.xyz'
-#input_file_path='/home/vanka/vipin/project_2_silane_aluminum_adduct/scanning_of_R_group_orientations/rc_for_ts_re_insertion_with_dcpdms_adduct_0_degree_dihedral.xyz'
+input_file_path='/home/vanka/vipin/project_2_silane_aluminum_adduct/scanning_of_R_group_orientations/rc_for_ts_re_insertion_with_dcpdms_adduct_0_degree_dihedral.xyz'
 
 output_dir_path='../output'
 
-d_theta=10
+d_theta=20
 
-ref_atom1_no=21
-ref_atom2_no=38
+ref_atom1_no=6
+ref_atom2_no=4
 
 atom_no_list="
-1
-2
-3
 4
+140
+3
+145
+137
 5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
+139
+141
+1
+143
+142
+2
+138
+144
 "
 
 
